314b45e5dddbe642ae4b1fe105d915167ea85703
Wang, Dawei <dwang@entertainment.com>
2018-08-21 16:18:00 -0400

    remove intermediate container

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HOST_UID=$(id -u)
 HOST_GID=$(id -g)
-docker image build --build-arg HOST_UID=${HOST_UID} --build-arg HOST_GID=${HOST_GID} -t 3dreconstruct:latest -f Dockerfile --force-rm  ./
+docker image build --rm --build-arg HOST_UID=${HOST_UID} --build-arg HOST_GID=${HOST_GID} -t 3dreconstruct:latest -f Dockerfile --force-rm  ./
