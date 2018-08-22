8b86a02909f5419c43ffe9c8f84bfc5520e11c79
Wang, Dawei <dwang@entertainment.com>
2018-08-22 15:03:34 -0400

    avoid send huge build context to docker daemon when build image

--- run_bash.sh
+++ run_bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 CPU_COUNT=$(cat /proc/cpuinfo |grep processor |wc -l)
 USE_CPUS=$((CPU_COUNT -2))
+SHAPENET_V1=/bigdata/study/shapenet/v1
 nvidia-docker run -it --rm --cpuset-cpus="0-${USE_CPUS}" \
  -v /tmp/.X11-unitx:/tmp./X11-unix -e DISPLAY=$DISPLAY \
- -v /bigdata/study/shapenet/v1:/shapenet \
- -v ${PWD}/run_data:/run_data \
+ -v ${SHAPENET_V1}:/shapenet \
  -v ${PWD}/code:/code \
  3dreconstruct:latest /bin/bash -l
