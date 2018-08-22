fb17bc191d442de830fc76845b6cb6386dd527e8
Wang, Dawei <dwang@entertainment.com>
2018-08-22 15:29:41 -0400

    check gpu device

--- docker/ts_gpu_info.py
+++ docker/ts_gpu_info.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+# this will output gpu that match capabilities
+tf.test.gpu_device_name()
 with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
